acac4a75bb1a4a9afe3326812cbb314d50c5c3b3
Hello <hello@mail.com>
2021-10-27 15:15:51 +0545

    Images desc Added

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ const App = () => {
     <>
       <Jumbotron.Container>
         {jumboData.map((item) => {
-          <Jumbotron key={item.id} direction={item.direction}>
-            <p>hello</p>
-          </Jumbotron>;
+          return (
+            <Jumbotron key={item.id} direction={item.direction}>
+              <Jumbotron.Pane>
+                <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+                <Jumbotron.Image src={item.image} alt={item.alt} />
+              </Jumbotron.Pane>
+            </Jumbotron>
+          );
         })}
       </Jumbotron.Container>
     </>
