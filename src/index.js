fd381e8294b3b12cd3b41736dabce4fe1768ff3b
Hello <hello@mail.com>
2021-10-27 17:27:02 +0545

    Added Black Theme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { render } from "react-dom";
 import App from "./App";
+import { GlobalStyles } from "./global-styles";
+import "normalize.css";
 
-render(<App />, document.getElementById("root"));
+render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
